21fa6ba5cbb363a1791eb9897c88967f72866522
Joseph Delcastillo III <delcastillo3j@gmail.com>
2018-07-26 10:09:05 -0400

    This is my initial code upload

---
+++ html/JS/main.js
@@ -0,0 +1,6 @@
+function init(){
+    var formCard = document.getElementById('formCard');
+    var cardHeader = document.getElementById('cardHeader');
+    var cardBody = document.getElementById('cardBody');
+
+}
